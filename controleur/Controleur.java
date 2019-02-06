c7e2eb9296ca2fe7079659041d9de4fc9b34ec76
Kuro <Kuro@Kuro>
2019-02-06 10:38:49 +0100

    ajout de quelques trucs

--- controleur/Controleur.java
+++ controleur/Controleur.java
@@ -12,17 +12,31 @@ import modele.Modele;
 public class Controleur implements EventHandler<ActionEvent> {
 	protected Modele modele;
 	protected Vue vue;
+	protected Regles regle;
+	protected boolean bouton1;
+	protected boolean bouton2;
+	protected boolean bouton3;
+	protected boolean bouton4;
+	protected boolean bouton5;
 	
 	public Controleur(Vue vue) {
 		this.modele = vue.getModele();
 		this.vue = vue;
+		this.regle = new regle();
+		this.bouton1 = false;
+		this.bouton2 = false;
+		this.bouton3 = false;
+		this.bouton4 = false;
+		this.bouton5 = false;
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
 		Object source = event.getSource();
-		if(vue.getCircle().contains((Circle)source)) {
+		if(vue.getCircles().contains((Circle)source)) {
+			if(regle.check_regle_choisie()) {
 			
+			}
 		}
 		if(vue.getSegment().contains((Line)source)) {
 			

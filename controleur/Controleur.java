9c2ff0bf6060ab3db34eca1e1458740bed065b90
Kuro <Kuro@Kuro>
2019-02-20 09:18:56 +0100

    j'ai enlevé des castes inutiles

--- controleur/Controleur.java
+++ controleur/Controleur.java
@@ -45,31 +45,31 @@ public class Controleur implements EventHandler<MouseEvent> {
 	public void handle(MouseEvent event) {
 		Object source = event.getSource();
 		if (source instanceof Circle && vue.getCercles().contains(source)) {
-			Point point = modele.getPoint(vue.getCercles().indexOf((Circle) source));
+			Point point = modele.getPoint(vue.getCercles().indexOf(source));
 			if (point instanceof PointCouleur && bouton == Bouton.COLORIER) {
 				((PointCouleur) point).setCouleur(modele.getJoueur(modele.getJoueurCourant()).getCouleur());
 			} else if (bouton == Bouton.SUPPRIMER) {
 				modele.removePoint(point);
 			} else if (bouton == Bouton.SEGMENT) {
 				if (premierPoint == null) {
-					premierPoint = modele.getPoint(vue.getCercles().indexOf((Circle) source));
+					premierPoint = modele.getPoint(vue.getCercles().indexOf(source));
 				} else {
 					this.modele.addSegment(new Segment(premierPoint, modele.getPoint(vue.getCercles().indexOf((Circle) source))));
 					premierPoint = null;
 				}
 			}
 		} else if (source instanceof Line && vue.getLignes().contains(source)) {
-			Segment segment = modele.getSegment(vue.getLignes().indexOf((Line) source));
+			Segment segment = modele.getSegment(vue.getLignes().indexOf(source));
 			if (segment instanceof SegmentCouleur && bouton == Bouton.COLORIER) {
 				((SegmentCouleur) segment).setCouleur(modele.getJoueur(modele.getJoueurCourant()).getCouleur());
 			} else if (bouton == Bouton.SUPPRIMER) {
-				modele.removeSegment(vue.getLignes().indexOf((Line) source));
+				modele.removeSegment(vue.getLignes().indexOf(source));
 			}
 		} else if (vue instanceof VueJeu && source instanceof Pane && ((VueJeu) vue).getGraphe() == source) {
 			if (bouton == Bouton.POINT) {
 				this.modele.addPoint(new Point(event.getX(), event.getY()));
 			}
-		} else if (source instanceof Button && vue.getBoutons().containsKey((Button) source)) {
+		} else if (source instanceof Button && vue.getBoutons().containsKey(source)) {
 			bouton = boutons.get(vue.getBoutons((Button) source));
 		}
 		vue.update();
